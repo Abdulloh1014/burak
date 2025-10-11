d679f7b2481670abee7568476d93c0b82affd212
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-10-11 21:48:05 +0900

    TASK - H bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,2 +1,10 @@
 console.log("Train ishga tushdi!");
 
+function getPositive(son: number[]) {
+    return son
+  .filter(raqam => raqam > 0 && Number.isInteger(raqam))
+  .join('');
+ }
+console.log("Javob:", JSON.stringify(getPositive([1, 7.3, 9, -5, 1.3, 9, -23, 9.1, 4, -12])));
+
+
