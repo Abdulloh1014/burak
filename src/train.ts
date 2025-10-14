e647332f9e5dc4c582387e03a2b76b2e50915517
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-10-14 23:39:48 +0900

    TASK - H2 bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,19 @@
 console.log("Train ishga tushdi!");
 
-function getPositive(son: number[]) {
+// function getPositive(son: number[]) {
+//     return son
+//   .filter(raqam => raqam > 0 && Number.isInteger(raqam))
+//   .join('');
+//  }
+// console.log("Javob:", JSON.stringify(getPositive([1, 7.3, 9, -5, 1.3, 9, -23, 9.1, 4, -12])));
+
+
+
+function getDigits(son: String) {
     return son
-  .filter(raqam => raqam > 0 && Number.isInteger(raqam))
+    .split('')           // split('') --> stingni harflarga bo'lyabti
+  .filter(r => /\d/.test(r))
   .join('');
  }
-console.log("Javob:", JSON.stringify(getPositive([1, 7.3, 9, -5, 1.3, 9, -23, 9.1, 4, -12])));
-
-
+console.log("Javob:", JSON.stringify(getDigits("a08b87d")));
 
