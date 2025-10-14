807ddd724461b11bad0878edd71797a4331f97c5
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-10-14 22:26:39 +0900

    feat: develop member schame model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,65 @@
+// member Schame
+
+import mongoose, {Schema} from 'mongoose';
+import { MemberType, MemberStatus} from '../libs/enums/member.enum';
+
+// Biz qurayotgan usul --- Schema first
+// Ikkinchi usul --- Code first
+
+
+const memberSchema = new Schema ({
+   memberType: {
+    type: String,
+    enum: MemberType,
+    defualt: MemberType.USER
+   },
+   
+   memberStatus: {
+    type: String,
+    enum: MemberStatus,
+    defsult: MemberStatus.ACTIVE
+  },
+
+  memberNick: {
+    type: String,
+    index: {unique: true, sparse: true},   // nicknameni takrorlanmas qiladi. Ya'ni databasega oldin yozilgan nickname takrorlansa ERROR beradi
+    required: true             // ma'lumotni kiritishini majburiy qiladi   
+  },                            // by default holatida (requiered:false) bo'ladi
+
+  memberPhone: {
+    type: String,
+    index: {unique: true, sparse: true},
+    required: true 
+  },
+
+  memberPassword: {
+    type: String,
+    select: true,    // database by defualt olib bermasin
+    required: true
+  },
+
+  memberAddres: {
+    type: String
+  },
+
+    memberDisc: {
+    type: String
+  },
+
+    memberImages: {
+    type: String
+  },
+
+    memberPoints: {
+    type: Number,
+    default: 0,
+  },
+
+},
+
+{ timestamps: true}     //createdAt va updatedAt ma'lumotlarini 
+                       // databasega avtomatic tushirib beradi
+
+);
+
+export default mongoose.model('Member', memberSchema);
