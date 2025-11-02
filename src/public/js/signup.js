1fc77c60df215996c2cb4890f4868e0bee460646
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-11-03 00:21:56 +0900

    Adminka signup jarayoni yakunlandi

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,7 +1,34 @@
+// const { name } = require("ejs");
+
 console.log("Signup frontend javascript file");
 
 
-$(function(){});
+$(function() {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function() {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpg, jpeg and png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+    }
+
+    $(this).siblings(".upload-name").val(filename);
+  });
+});
 
 
 
@@ -26,4 +53,14 @@ function validateSignupForm() {
         alert("Password differs, please ckeck!");
         return false;
     }
+
+    const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+    if (!memberImage) {
+        alert("Please insert restaurant image!");
+        return false;
+    }
+
+
 }
