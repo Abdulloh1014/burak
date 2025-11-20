e3fc1ed0ff3a98ddb8e2af190baf2ffdb8db86c4
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-11-20 23:51:40 +0900

    fix: modify getMemberDetail business logic

--- src/router.ts
+++ src/router.ts
@@ -14,7 +14,10 @@ router.post(
     memberController.logout, 
     
 );
-router.get("/member/detail", memberController.verifyAuth);
+router.get("/member/detail", 
+    memberController.verifyAuth,
+   memberController.getMemberDetail
+);
 
 
 /** Product */
