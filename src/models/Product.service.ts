0f9dcedb811f1be16cc6eb8e78b16fe873db2963
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-11-25 23:48:31 +0900

    fix: modify getProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -5,14 +5,19 @@ import { Product, ProductInput, ProductInquiry, ProductUpdateInput } from "../li
 import ProductModel from "../schema/Product.model";
 import { T } from "../libs/types/comman";
 import { ObjectId} from "mongoose";
+import ViewService from "./View.service";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view.enum";
 
 
 
 class ProductService {
   private readonly productModel;
+  public viewService;
 
   constructor() {
     this.productModel = ProductModel;
+    this.viewService = new ViewService()
   }
 
   /** SPA **/
@@ -47,7 +52,7 @@ class ProductService {
 
 
 
-  public async getProduct( member: ObjectId | null, id: string): Promise<Product> {
+  public async getProduct( memberId: ObjectId | null, id: string): Promise<Product> {
      const productId = shapeIntoMongooseObjectId(id);
 
      let result = await this.productModel
@@ -58,9 +63,33 @@ class ProductService {
      .exec();
      if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
+     
+    if(memberId) {
+      // Check  Existence
+      const input: ViewInput = { 
+        memberId: memberId,
+        viewRefId: productId,
+        viewGroup: ViewGroup.PRODUCT
+      }
+     const existView = await this.viewService.checkViewExistance(input)
+       
+     console.log("exsit:", !!existView)
+       if(!existView){
+        // Insert View
+        await this.viewService.insertMemberView(input);
+         
+        // Increace  Counts
+         result= await this.productModel
+        .findByIdAndUpdate(
+          productId,
+          { $inc: { productViews: +1 }},
+          { new: true }
+        )
+        .exec();
 
-     // TODO:  if authenticated users => first => view log creation 
-
+      }
+    }
+     
 
      return result;
      
