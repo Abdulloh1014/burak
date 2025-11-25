fe51b81d6149c12567257755526da5d8126decff
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-11-25 21:18:35 +0900

    feat: develop getProducts rest Api

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,7 +4,8 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/comman";
 import ProductService from "../models/Product.service";
 import { AdminRequest } from "../libs/types/member";
-import { ProductInput } from "../libs/types/product";
+import { ProductInput, ProductInquiry } from "../libs/types/product";
+import { ProductCollection } from "../libs/enums/product.enum";
 
 
 const productService = new ProductService();
@@ -13,6 +14,29 @@ const productController: T = {};
 
   /** SPA **/
 
+  productController.getProducts = async (req: Request, res: Response) => {
+    try {
+        console.log("getProducts");
+        const {page, limit, order, productCollection, search } = req.query;
+        const inquiry: ProductInquiry = {
+            order: String(order),
+            page: Number(page),
+            limit: Number(limit),
+        };
+        if(productCollection) {
+            inquiry.productCollection = productCollection as ProductCollection;
+        }
+        if(search) inquiry.search = String(search);
+        
+        const result = await productService.getProduct(inquiry);
+        
+       res.status(HttpCode.OK).json(result)
+    } catch(err) {
+        console.log("Error, getProducts", err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart)
+    }
+  }
 
     /** SSR **/
 
