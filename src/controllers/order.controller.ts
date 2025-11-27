e09b35d4f57af90483c6c6cfd3fb1dbe929b09df
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-11-28 00:33:12 +0900

    feat: develop fetMyOrders rest Api

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,11 +3,15 @@ import { T } from "../libs/types/comman";
 import { Response } from "express";
 import Errors, { HttpCode } from "../libs/Errors";
 import OrderService from "../models/Order.service";
+import { OrderInquiry } from "../libs/types/order";
+import { OrderStatus } from "../libs/enums/order.enum";
 
 const orderService = new OrderService();
 
-
 const orderController: T = {};
+
+
+
 orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
    try {
         console.log("createOrder");
@@ -17,6 +21,27 @@ orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
 
    } catch(err) {
         console.log("Error, getRestaurant", err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart)
+    }
+}
+
+orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
+   try {
+        console.log("getMyOrders");
+       const { page, limit, orderStatus } = req.query;
+        const inquiry: OrderInquiry = {
+         page: Number(page),
+         limit: Number(limit),
+         orderStatus: orderStatus as OrderStatus
+        };
+        console.log("inquiry:", inquiry);
+        const result = await orderService.getMyOrders(req.member, inquiry);
+    
+        res.status(HttpCode.CREATED).json(result);
+
+   } catch(err) {
+        console.log("Error, getMyOrders", err);
         if(err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standart.code).json(Errors.standart)
     }
--- src/libs/types/order.ts
+++ src/libs/types/order.ts
@@ -2,6 +2,7 @@
 
 import { ObjectId} from "mongoose";
 import { OrderStatus } from "../enums/order.enum";
+import { Product } from "./product";
 
 
 export interface OrderItem {
@@ -22,6 +23,9 @@ export interface Order {
     memberId: ObjectId;
     createdAt: Date;
     updatedAt: Date;
+    /** from aggregations  */
+    orderItems:  OrderItem[];
+    productData: Product[];
 }
 
 
@@ -30,4 +34,10 @@ export interface OrderItemInput {
     itemPrice: number;
     productId: ObjectId;
     orderId: ObjectId;
+}
+
+export interface OrderInquiry {
+    page: number;
+    limit: number;
+    orderStatus: OrderStatus;
 }
